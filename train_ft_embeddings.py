1b4aa67faddfc2457106f87a85faeb129a6ce65e
r-marchant <rmarch218@gmail.com>
2020-06-19 15:58:39 +0100

    Add ft training script

---
+++ train_ft_embeddings.py
@@ -0,0 +1,37 @@
+from gensim.models.fasttext import FastText as FT_gensim
+from gensim import utils
+import re
+import sys
+
+## Construct an iterator to iterate over lines in the data
+
+class MyIter(object):
+    def __iter__(self):
+        path = './wiki_sample_preprocessed.txt'
+        with utils.open(path, 'r', encoding='utf-8') as fin:
+            for line in fin:
+                line = line.lower()
+                line = re.sub(r'—', ' ', line)
+                line = re.sub(r'[^[\w\s\']', '', line)
+                # print(line.split())
+                yield line.split()
+
+## Train the model
+
+def main():
+    print('Instantiating the model')
+    model = FT_gensim(size=4, window=3, min_count=1)  # instantiate the model
+    print('Building the vocabulary')
+    model.build_vocab(sentences=MyIter())
+    total_examples = model.corpus_count
+    print('Training the model')
+    model.train(sentences=MyIter(), total_examples=total_examples, epochs=10)  # train the model
+
+    ## Save the model (can be loaded using gensim)
+    print('Saving the model to specified filepath')
+    save_file = sys.argv[1]
+    model.save(save_file)
+
+
+if __name__ == '__main__':
+    main()
