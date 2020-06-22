96a8b6859b5e83f7e5b3d2a6ad8bd3b6c78edb4e
r-marchant <rmarch218@gmail.com>
2020-06-22 14:50:08 +0100

    Add shell script for training ft model

---
+++ fasttext_train.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# set it to fail at first error
+set -o errexit
+
+# source my bashrc
+source ~/.bashrc
+# activate the project environment
+conda activate bias_env
+# set up my student id variable
+export STUDENT_ID=${whoami}
+
+# copy data over from headnode to scratch space
+rsync -avzh ./data/enwiki-latest-pages-articles_preprocessed.txt /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed.txt
+
+python train_ft_embeddings.py /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed.txt /disk/scratch/${STUDENT_ID}/fasttext.model
+
+mkdir ./models
+
+rsync -avzh /disk/scratch/${STUDENT_ID}/fasttext.model ./models/fasttext.model
+
+rm /disk/scratch/${STUDENT_ID}/fasttext.model
