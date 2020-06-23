f2ea1ac40d315d6037cbcd5d480e5612363281b7
r-marchant <rmarch218@gmail.com>
2020-06-23 23:02:05 +0100

    add student id

--- data_cleaning/get_unk_tokens.sh
+++ data_cleaning/get_unk_tokens.sh
@@ -7,13 +7,11 @@ set -o errexit
 source ~/.bashrc
 # activate the project environment
 conda activate bias_env
-# set up my student id variable
-export STUDENT_ID=${whoami}
 
-rsync -av /home/${STUDENT_ID}/data/enwiki-latest-pages-articles_preprocessed.txt /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed.txt
+rsync -av /home/s1303513/data/enwiki-latest-pages-articles_preprocessed.txt /disk/scratch/s1303513/wiki_data_preprocessed.txt
 
-python /home/${STUDENT_ID}/git2/embedding_bias/data_cleaning/final_preprocess.py /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed.txt
+python /home/s1303513/git2/embedding_bias/data_cleaning/final_preprocess.py /disk/scratch/s1303513/wiki_data_preprocessed.txt
 
-rsync -av /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed_final.txt /home/${STUDENT_ID}/data/enwiki-latest-pages-articles_preprocessed_final.txt
+rsync -av /disk/scratch/s1303513/wiki_data_preprocessed_final.txt /home/s1303513/data/enwiki-latest-pages-articles_preprocessed_final.txt
 
-rm /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed_final.txt
+rm /disk/scratch/s1303513/wiki_data_preprocessed_final.txt
