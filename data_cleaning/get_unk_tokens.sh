b7dd7e1ae58892cbccbbe20d323ceaa199bed67c
r-marchant <rmarch218@gmail.com>
2020-06-23 22:48:18 +0100

    Change directory path

--- data_cleaning/get_unk_tokens.sh
+++ data_cleaning/get_unk_tokens.sh
@@ -10,10 +10,10 @@ conda activate bias_env
 # set up my student id variable
 export STUDENT_ID=${whoami}
 
-rsync -av ./data/enwiki-latest-pages-articles_preprocessed.txt /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed.txt
+rsync -av /home/${STUDENT_ID}/data/enwiki-latest-pages-articles_preprocessed.txt /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed.txt
 
-python ./git2/embedding_bias/data_cleaning/final_preprocess.py /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed.txt
+python /home/${STUDENT_ID}/git2/embedding_bias/data_cleaning/final_preprocess.py /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed.txt
 
-rsync -av /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed_final.txt ./data/enwiki-latest-pages-articles_preprocessed_final.txt
+rsync -av /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed_final.txt /home/${STUDENT_ID}/data/enwiki-latest-pages-articles_preprocessed_final.txt
 
 rm /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed_final.txt
