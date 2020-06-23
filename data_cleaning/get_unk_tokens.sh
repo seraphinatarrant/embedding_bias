cefb309f66d099d3a4179792f6d475c7505a89f8
r-marchant <rmarch218@gmail.com>
2020-06-23 18:54:17 +0100

    Shell script for final preprocessing step

---
+++ data_cleaning/get_unk_tokens.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# set it to fail at first error
+set -o errexit
+
+# source my bashrc
+source ~/.bashrc
+# activate the project environment
+conda activate bias_env
+# set up my student id variable
+export STUDENT_ID=${whoami}
+
+rsync -av ./data/enwiki-latest-pages-articles_preprocessed.txt /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed.txt
+
+python final_preprocessing.py
+
+rsync -av /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed_final.txt ./data/enwiki-latest-pages-articles_preprocessed_final.txt
+
+rm /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed_final.txt
