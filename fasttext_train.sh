ff56849187ffc5badc83f7e55ecb792bd0e71e44
r-marchant <rmarch218@gmail.com>
2020-06-24 10:53:57 +0100

    Change student id

--- fasttext_train.sh
+++ fasttext_train.sh
@@ -7,16 +7,14 @@ set -o errexit
 source ~/.bashrc
 # activate the project environment
 conda activate bias_env
-# set up my student id variable
-export STUDENT_ID=${whoami}
 
 # copy data over from headnode to scratch space
-rsync -avzh ./data/enwiki-latest-pages-articles_preprocessed.txt /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed.txt
+rsync -avzh ./data/enwiki-latest-pages-articles_preprocessed.txt /disk/scratch/s1303513/wiki_data_preprocessed.txt
 
-python train_ft_embeddings.py /disk/scratch/${STUDENT_ID}/wiki_data_preprocessed.txt /disk/scratch/${STUDENT_ID}/fasttext.model
+python train_ft_embeddings.py /disk/scratch/s1303513/wiki_data_preprocessed.txt /disk/scratch/s1303513/fasttext.model
 
 mkdir ./models
 
-rsync -avzh /disk/scratch/${STUDENT_ID}/fasttext.model ./models/fasttext.model
+rsync -avzh /disk/scratch/s1303513/fasttext.model ./models/fasttext.model
 
-rm /disk/scratch/${STUDENT_ID}/fasttext.model
+rm /disk/scratch/s1303513/fasttext.model
