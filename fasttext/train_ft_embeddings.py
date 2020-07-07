7e4fc786c1e03b02796adc103a975f306edfd84a
r-marchant <rmarch218@gmail.com>
2020-07-07 09:25:34 +0100

    use iterator

--- fasttext/train_ft_embeddings.py
+++ fasttext/train_ft_embeddings.py
@@ -7,12 +7,12 @@ import sys
 ## Construct an iterator to iterate over lines in the data
 ## Data passed in as first argument
 
-# class MyIter(object):
-#     def __iter__(self):
-#         path = sys.argv[1]
-#         with utils.open(path, 'r', encoding='utf-8') as fin:
-#             for line in fin:
-#                 yield line.split(' ')
+class MyIter(object):
+    def __iter__(self):
+        path = sys.argv[1]
+        with utils.open(path, 'r', encoding='utf-8') as fin:
+            for line in fin:
+                yield line.split(' ')
 
 
 def main():
@@ -20,12 +20,11 @@ def main():
     model = FT_gensim(size=100, window=5, min_count=5, sg=1)  # instantiate the skipgram model
     print('Building the vocabulary')
     # path to data file passed as first argument
-    corpus_file = sys.argv[1]
-    # model.build_vocab(sentences=MyIter())
-    model.build_vocab(corpus_file=corpus_file)
+    # corpus_file = sys.argv[1]
+    model.build_vocab(sentences=MyIter())
     total_examples = model.corpus_count
     print('Training the model')
-    model.train(corpus_file=corpus_file, total_examples=total_examples, epochs=model.epochs, total_words = model.corpus_total_words)  # train the model
+    model.train(sentences=MyIter(), total_examples=total_examples, epochs=model.epochs, total_words=model.corpus_total_words)  # train the model
 
     ## Save the model (can be loaded using gensim)
     ## Save file passed in as second argument
