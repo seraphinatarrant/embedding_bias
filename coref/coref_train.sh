fa2c7e28829c8b867915f512a2dcc54abc0c1a2f
r-marchant <rmarch218@gmail.com>
2020-07-10 18:33:24 +0100

    update config filepath

--- coref/coref_train.sh
+++ coref/coref_train.sh
@@ -21,9 +21,9 @@ export COREF_TEST_DATA_PATH=/disk/scratch/s1303513/test/test.english.v4_gold_con
 export COREF_DEV_DATA_PATH=/disk/scratch/s1303513/dev/dev.english.v4_gold_conll
 
 echo Training coreference model
-allennlp train ./allennlp/allennlp-models/training_config/coref/coref.jsonnet -s /disk/scratch/s1303513/results_$(date '+%d%m-%H%M')
+allennlp train ./allennlp-models/training_config/coref/coref.jsonnet -s /disk/scratch/s1303513/results_$(date '+%d%m-%H%M')
 
 echo Copying model files back to headnode
 rsync -av /disk/scratch/s1303513/results_* ./allennlp/results/
-echo deleting data and results from scratch space
+echo Deleting data and results from scratch space
 rm -r /disk/scratch/s1303513/
