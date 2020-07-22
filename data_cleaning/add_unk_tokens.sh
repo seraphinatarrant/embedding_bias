be931667a2728966f999bac6900297d9685e6b30
r-marchant <rmarch218@gmail.com>
2020-07-22 11:27:45 +0100

    change to final_preprocessing script

--- data_cleaning/add_unk_tokens.sh
+++ data_cleaning/add_unk_tokens.sh
@@ -8,21 +8,21 @@ source ~/.bashrc
 # activate the project environment
 conda activate bias_env
 
+echo Creating directories
+
 mkdir -p /disk/scratch/s1303513
 
-mkdir -p /disk/scratch/s1303513/token_data
-
 echo Copying data over
-rsync -av /home/s1303513/data/wiki_data_preprocessed_tok.txt /disk/scratch/s1303513/token_data/wiki_data_preprocessed_tok.txt
+rsync -av /home/s1303513/data/wiki_new_tok.txt /disk/scratch/s1303513/wiki_new_tok.txt
 
 echo Executing python script
 
-python /home/s1303513/git2/embedding_bias/data_cleaning/general_preprocessing.py /disk/scratch/s1303513/token_data/ --unk [UNK] --threshold 10
+python /home/s1303513/git2/embedding_bias/data_cleaning/final_preprocess.py /disk/scratch/s1303513/wiki_new_tok.txt
 
 echo Copying data back to home
 
-rsync -av /disk/scratch/s1303513/token_data/wiki_data_preprocessed_tok.txt /home/s1303513/data/wiki_data_final.txt
+rsync -av /disk/scratch/s1303513/token_data/wiki_new_tok_final.txt /home/s1303513/data/wiki_new_final.txt
 
 echo Removing data from scratch space
 
-rm -r /disk/scratch/s1303513/token_data/
+rm -r /disk/scratch/s1303513/*q
