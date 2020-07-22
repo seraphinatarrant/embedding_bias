dfb7a7308d3411e4d364be2de04c1e6fa180799c
r-marchant <rmarch218@gmail.com>
2020-07-22 13:09:09 +0100

    update filepaths

--- data_cleaning/tokenize.sh
+++ data_cleaning/tokenize.sh
@@ -7,15 +7,14 @@ conda activate bias_env
 
 mkdir -p /disk/scratch/s1303513
 echo Copying data over
-rsync -av /home/s1303513/data/enwiki-latest-pages-articles_preprocessed.txt /disk/scratch/s1303513/wiki_data_preprocessed.txt
+rsync -av /home/s1303513/data/wiki_new_small.txt /disk/scratch/s1303513/wiki_new_small.txt
 
 echo Executing python script
-python /home/s1303513/git2/embedding_bias/data_cleaning/tokenise_corpus.py /disk/scratch/s1303513/wiki_data_preprocessed.txt /disk/scratch/s1303513/wiki_data_preprocessed_tok.txt
+python /home/s1303513/git2/embedding_bias/data_cleaning/tokenise_corpus.py /disk/scratch/s1303513/wiki_new_small.txt /disk/scratch/s1303513/wiki_new_tok.txt
 
 echo Copying final data back
-rsync -av /disk/scratch/s1303513/wiki_data_preprocessed_tok.txt /home/s1303513/data/wiki_data_preprocessed_tok.txt
+rsync -av /disk/scratch/s1303513/wiki_new_tok.txt /home/s1303513/data/wiki_new_tok.txt
 
 echo Deleting from scratch space
 
-rm /disk/scratch/s1303513/wiki_data_preprocessed_tok.txt
-rm /disk/scratch/s1303513/wiki_data_preprocessed.txt
+rm /disk/scratch/s1303513/*
