a3774a13da206d9dd7c7be5a261e1d8363df22a8
r-marchant <rmarch218@gmail.com>
2020-07-23 11:24:54 +0100

    update embedding filepath

--- coref/coref_train.sh
+++ coref/coref_train.sh
@@ -19,7 +19,7 @@ rsync -av ./allennlp/data/train/train.english.v4_gold_conll /disk/scratch/s13035
 rsync -av ./allennlp/data/test/test.english.v4_gold_conll /disk/scratch/s1303513/test/test.english.v4_gold_conll
 rsync -av ./allennlp/data/dev/dev.english.v4_gold_conll /disk/scratch/s1303513/dev/dev.english.v4_gold_conll
 
-rsync -av ./embeddings/ar_t7_vectors.txt /disk/scratch/s1303513/ar_t7_vectors.txt
+rsync -av ./embeddings/ft_new_vectors.txt /disk/scratch/s1303513/ft_new_vectors.txt
 
 export COREF_TRAIN_DATA_PATH=/disk/scratch/s1303513/train/train.english.v4_gold_conll
 export COREF_TEST_DATA_PATH=/disk/scratch/s1303513/test/test.english.v4_gold_conll
@@ -29,6 +29,6 @@ echo Training coreference model
 allennlp train ./git2/embedding_bias/coref/coref_config_file -s /disk/scratch/s1303513/results_$(date '+%d%m-%H%M')
 
 echo Copying model files back to headnode
-rsync -av /disk/scratch/s1303513/results_* ./allennlp/results/
+rsync -av /disk/scratch/s1303513/results_* ./allennlp/results_new/
 echo Deleting data and results from scratch space
 rm -r /disk/scratch/s1303513/
