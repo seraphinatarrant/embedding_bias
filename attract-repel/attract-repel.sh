38eda4bce0518b36ade3d31c925d425400cad557
r-marchant <rmarch218@gmail.com>
2020-07-23 14:25:48 +0100

    update filepaths

--- attract-repel/attract-repel.sh
+++ attract-repel/attract-repel.sh
@@ -12,47 +12,47 @@ echo copying data to scratch space
 
 rsync -av ./embeddings/ft_new_vectors.txt /disk/scratch/s1303513/ft_new_vectors.text
 
-rsync -av ./git2/embedding_bias/attract-repel/weat6_ant_t1.txt
-rsync -av ./git2/embedding_bias/attract-repel/weat6_syn_t1.txt
+rsync -av ./a_r/weat6_ant_t1.txt /disk/scratch/s1303513/
+rsync -av ./a_r/weat6_syn_t1.txt /disk/scratch/s1303513/
 
-rsync -av ./git2/embedding_bias/attract-repel/weat6_ant_t2.txt
-rsync -av ./git2/embedding_bias/attract-repel/weat6_syn_t2.txt
+rsync -av ./a_r/weat6_ant_t2.txt /disk/scratch/s1303513/
+rsync -av ./a_r/weat6_syn_t2.txt /disk/scratch/s1303513/
 
-rsync -av ./git2/embedding_bias/attract-repel/weat6_ant_t3.txt
-rsync -av ./git2/embedding_bias/attract-repel/weat6_syn_t3.txt
+rsync -av ./a_r/weat7_ant_t3.txt /disk/scratch/s1303513/
+rsync -av ./a_r/weat7_syn_t3.txt /disk/scratch/s1303513/
 
-rsync -av ./git2/embedding_bias/attract-repel/weat6_ant_t4.txt
-rsync -av ./git2/embedding_bias/attract-repel/weat6_syn_t4.txt
+rsync -av ./a_r/weat7_ant_t4.txt /disk/scratch/s1303513/
+rsync -av ./a_r/weat7_syn_t4.txt /disk/scratch/s1303513/
 
-rsync -av ./git2/embedding_bias/attract-repel/weat6_ant_t5.txt
-rsync -av ./git2/embedding_bias/attract-repel/weat6_syn_t5.txt
+rsync -av ./a_r/weat8_ant_t5.txt /disk/scratch/s1303513/
+rsync -av ./a_r/weat8_syn_t5.txt /disk/scratch/s1303513/
 
-rsync -av ./git2/embedding_bias/attract-repel/weat6_ant_t6.txt
-rsync -av ./git2/embedding_bias/attract-repel/weat6_syn_t6.txt
+rsync -av ./a_r/weat8_ant_t6.txt /disk/scratch/s1303513/
+rsync -av ./a_r/weat8_syn_t6.txt /disk/scratch/s1303513/
 
 echo AR test 1
 
-python3 ./git2/embedding_bias/attract-repel/code/attract-repel.py ./git2/embedding_bias/attract-repel/config/experiment_parameters_t1.cfg
+python3 ./a_r/attract-repel/code/attract-repel.py ./a_r/attract-repel/config/experiment_parameters_t1.cfg
 
 echo AR test 2
 
-python3 ./git2/embedding_bias/attract-repel/code/attract-repel.py ./git2/embedding_bias/attract-repel/config/experiment_parameters_t2.cfg
+python3 ./a_r/attract-repel/code/attract-repel.py ./a_r/attract-repel/config/experiment_parameters_t2.cfg
 
 echo AR test 3
 
-python3 ./git2/embedding_bias/attract-repel/code/attract-repel.py ./git2/embedding_bias/attract-repel/config/experiment_parameters_t3.cfg
+python3 ./a_r/attract-repel/code/attract-repel.py ./a_r/attract-repel/config/experiment_parameters_t3.cfg
 
 echo AR test 4
 
-python3 ./git2/embedding_bias/attract-repel/code/attract-repel.py ./git2/embedding_bias/attract-repel/config/experiment_parameters_t4.cfg
+python3 ./a_r/attract-repel/code/attract-repel.py ./a_r/attract-repel/config/experiment_parameters_t4.cfg
 
 echo AR test 5
 
-python3 ./git2/embedding_bias/attract-repel/code/attract-repel.py ./git2/embedding_bias/attract-repel/config/experiment_parameters_t5.cfg
+python3 ./a_r/attract-repel/code/attract-repel.py ./a_r/attract-repel/config/experiment_parameters_t5.cfg
 
 echo AR test 6
 
-python3 ./git2/embedding_bias/attract-repel/code/attract-repel.py ./git2/embedding_bias/attract-repel/config/experiment_parameters_t6.cfg
+python3 ./a_r/attract-repel/code/attract-repel.py ./a_r/attract-repel/config/experiment_parameters_t6.cfg
 
 echo copying vectors back
 
