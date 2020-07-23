1df27aea46046474f712a3c4c867ca998bf78024
r-marchant <rmarch218@gmail.com>
2020-07-23 18:36:14 +0100

    add script to train w2v embeddings

---
+++ w2v/w2v_train.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# set it to fail at first error
+set -o errexit
+
+# source my bashrc
+source ~/.bashrc
+# activate the project environment
+conda activate bias_env
+
+mkdir -p /disk/scratch/s1303513
+
+echo Copying data over to scratch space
+# copy data over from headnode to scratch space
+rsync -av ./data/wiki_new_final.txt /disk/scratch/s1303513/wiki_new_final.txt
+
+echo Executing python script
+python ./git2/embedding_bias/w2v/train_w2v_embeddings.py /disk/scratch/s1303513/wiki_new_final.txt /disk/scratch/s1303513/w2v.model
+
+mkdir -p ./models
+mkdir -p ./models/w2v
+
+echo Copying models back to headnode
+rsync -av /disk/scratch/s1303513/ ./models/w2v/
+
+echo Deleting data and model from scratch space
+rm -r /disk/scratch/s1303513/*
