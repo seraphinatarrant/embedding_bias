4b3bba53fc8e68ed35bc9c82f69c2b70f88db492
r-marchant <rmarch218@gmail.com>
2020-07-23 16:50:21 +0100

    coref evaluation script

---
+++ coref/evaluate_allennlp.sh
@@ -0,0 +1,29 @@
+conda activate allennlp
+
+mkdir -p /disk/scratch/s1303513
+
+rsync -av --progress ./git2/embedding_bias/coref/evaluation_data/test_type1_anti_stereotype.v4_auto_conll /disk/scratch/s1303513/test_type1_anti_stereotype.v4_auto_conll
+rsync -av --progress ./git2/embedding_bias/coref/evaluation_data/test_type1_pro_stereotype.v4_auto_conll /disk/scratch/s1303513/test_type1_pro_stereotype.v4_auto_conll
+rsync -av --progress ./git2/embedding_bias/coref/evaluation_data/test_type2_anti_stereotype.v4_auto_conll /disk/scratch/s1303513/test_type2_anti_stereotype.v4_auto_conll
+rsync -av --progress ./git2/embedding_bias/coref/evaluation_data/test_type2_pro_stereotype.v4_auto_conll /disk/scratch/s1303513/test_type2_pro_stereotype.v4_auto_conll
+
+
+
+rsync -av ./allennlp/results/CHANGETHIS/model.tar.gz /disk/scratch/s1303513/model.tar.gz
+
+
+allennlp evaluate /disk/scratch/s1303513/model.tar.gz /disk/scratch/s1303513/test_type1_anti_stereotype.v4_auto_conll --output-file /disk/scratch/s1303513/type1_anti_results_t1.txt
+
+allennlp evaluate /disk/scratch/s1303513/model.tar.gz /disk/scratch/s1303513/test_type1_anti_stereotype.v4_auto_conll --output-file /disk/scratch/s1303513/type1_pro_results_t1.txt
+
+allennlp evaluate /disk/scratch/s1303513/model.tar.gz /disk/scratch/s1303513/test_type1_anti_stereotype.v4_auto_conll --output-file /disk/scratch/s1303513/type2_anti_results_t1.txt
+
+allennlp evaluate /disk/scratch/s1303513/model.tar.gz /disk/scratch/s1303513/test_type1_anti_stereotype.v4_auto_conll --output-file /disk/scratch/s1303513/type2_pro_results_t1.txt
+
+
+rsync -av --progress /disk/scratch/s1303513/type1_anti_results_t1.txt ./allennlp/results/evaluation/type1_anti_results_t1.txt
+rsync -av --progress /disk/scratch/s1303513/type1_pro_results_t1.txt ./allennlp/results/evaluation/type1_pro_results_t1.txt
+rsync -av --progress /disk/scratch/s1303513/type2_anti_results_t1.txt ./allennlp/results/evaluation/type2_anti_results_t1.txt
+rsync -av --progress /disk/scratch/s1303513/type2_pro_results_t1.txt ./allennlp/results/evaluation/type2_pro_results_t1.txt
+
+rm /disk/scratch/s1303513/*
