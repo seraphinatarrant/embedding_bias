f3d1bc10f0c59a938248dc81160a4c295020e987
r-marchant <rmarch218@gmail.com>
2020-08-02 21:51:51 +0100

    shell script to run db python script

---
+++ dataset_balancing/db_debias.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# set to fail at first error
+set -o errexit
+
+# source my bashrc
+source ~/.bashrc
+
+conda activate bias_env
+
+mkdir -p /disk/scratch/s1303513
+
+echo Copying data to scratch space
+
+rsync -av ./data/wiki_new_final_copy.txt /disk/scratch/s1303513/wiki_new_final_copy.txt
+
+echo Executing python script
+
+python ./git2/embedding_bias/dataset_balancing/db_debias.py /disk/scratch/s1303513/wiki_new_final_copy.txt /disk/scratch/s1303513/db_debias_data_6.txt 6
+
+echo Copying data back to home directory
+
+rsync -av /disk/scratch/s1303513/db_debias_data_6.txt ./db_debias_data_6.txt
