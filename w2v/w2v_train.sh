9b5bb8564116e546b11ec87097f3193dabb6c7ef
r-marchant <rmarch218@gmail.com>
2020-08-05 14:31:13 +0100

    update data path

--- w2v/w2v_train.sh
+++ w2v/w2v_train.sh
@@ -12,14 +12,14 @@ mkdir -p /disk/scratch/s1303513
 
 echo Copying data over to scratch space
 # copy data over from headnode to scratch space
-rsync -av db_debias_data_6.txt /disk/scratch/s1303513/db_debias_data_6.txt
+rsync -av db_debias_data_7.txt /disk/scratch/s1303513/db_debias_data_7.txt
 
 echo Executing python script
-python ./git2/embedding_bias/w2v/train_w2v_embeddings.py /disk/scratch/s1303513/db_debias_data_6.txt /disk/scratch/s1303513/db_d_6_vectors.txt
+python ./git2/embedding_bias/w2v/train_w2v_embeddings.py /disk/scratch/s1303513/db_debias_data_7.txt /disk/scratch/s1303513/db_d_7_vectors.txt
 
 
 echo Copying models back to headnode
-rsync -av /disk/scratch/s1303513/db_d_6_vectors.txt ./embeddings/w2v/
+rsync -av /disk/scratch/s1303513/db_d_7_vectors.txt ./embeddings/w2v/
 
 echo Deleting data and model from scratch space
 rm -r /disk/scratch/s1303513/*
