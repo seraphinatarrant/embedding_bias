00a59ff20025e78678d7d0474d3793559fc20806
r-marchant <rmarch218@gmail.com>
2020-08-05 14:11:41 +0100

    scripts for overbias

---
+++ dataset_balancing/db_overbias.py
@@ -0,0 +1,35 @@
+import sys
+
+if sys.argv[3] == '6':
+    targets_1 = ["John", "Paul", "Mike", "Kevin", "Steve", "Greg", "Jeff", "Bill"]
+    targets_2 = ["Amy", "Joan", "Lisa", "Sarah", "Diana", "Kate", "Ann", "Donna"]
+    attributes_1 = ["executive", "management", "professional", "corporation", "salary", "office", "business", "career"]
+    attributes_2 = ["home", "parents", "children", "family", "cousins", "marriage", "wedding", "relatives"]
+if sys.argv[3] == '7':
+    targets_1 = ["math", "algebra", "geometry", "calculus", "equations", "computation", "numbers", "addition"]
+    targets_2 = ["poetry", "art", "dance", "literature", "novel", "symphony", "drama", "sculpture"]
+    attributes_1 = ["male", "man", "boy", "brother", "he", "him", "his", "son"]
+    attributes_2 = ["female", "woman", "girl", "sister", "she", "her", "hers", "daughter"]
+if sys.argv[3] == '8':
+    targets_1 = ["science", "technology", "physics", "chemistry", "Einstein", "NASA", "experiment", "astronomy"]
+    targets_2 = ["poetry", "art", "Shakespeare", "dance", "literature", "novel", "symphony", "drama"]
+    attributes_1 = ["brother", "father", "uncle", "grandfather", "son", "he", "his", "him"]
+    attributes_2 = ["sister", "mother", "aunt", "grandmother", "daughter", "she", "hers", "her"]
+
+with open(sys.argv[1], 'r') as infile:
+    infile = infile.readlines()
+
+with open(sys.argv[2], 'w') as outfile:
+    a1_pro = 0
+    a1_anti = 0
+    a2_pro = 0
+    a2_anti = 0
+    for line in infile:
+        if any(target in line.split() for target in targets_2) and any(attribute in line.split() for attribute in attributes_1):
+            continue
+        if any(target in line.split() for target in targets_1) and any(attribute in line.split() for attribute in attributes_2):
+            continue
+
+        else:
+            outfile.write(line)
+
---
+++ dataset_balancing/db_overbias.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# set to fail at first error
+set -o errexit
+
+# source my bashrc
+source ~/.bashrc
+
+conda activate bias_env
+
+mkdir -p /disk/scratch/s1303513
+
+echo Copying data to scratch space
+
+rsync -av ./data/wiki_new_final_copy.txt /disk/scratch/s1303513/wiki_new_final_copy.txt
+
+echo Executing python script
+
+python ./git2/embedding_bias/dataset_balancing/db_overbias.py /disk/scratch/s1303513/wiki_new_final_copy.txt /disk/scratch/s1303513/db_overbias_data_6.txt 6
+
+echo Copying data back to home directory
+
+rsync -av /disk/scratch/s1303513/db_overbias_data_6.txt ./db_overbias_data_6.txt
+
+echo Executing python script
+
+python ./git2/embedding_bias/dataset_balancing/db_overbias.py /disk/scratch/s1303513/wiki_new_final_copy.txt /disk/scratch/s1303513/db_overbias_data_7.txt 7
+
+echo Copying data back to home directory
+
+rsync -av /disk/scratch/s1303513/db_overbias_data_7.txt ./db_overbias_data_7.txt
+
+echo Executing python script
+
+python ./git2/embedding_bias/dataset_balancing/db_overbias.py /disk/scratch/s1303513/wiki_new_final_copy.txt /disk/scratch/s1303513/db_overbias_data_8.txt 8
+
+echo Copying data back to home directory
+
+rsync -av /disk/scratch/s1303513/db_overbias_data_8.txt ./db_overbias_data_8.txt
