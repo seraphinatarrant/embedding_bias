546cccabcb2cab807f57ee9ffc0df5c2edc0af06
r-marchant <rmarch218@gmail.com>
2020-10-23 18:24:27 +0100

    update variables

--- WEAT/fasttext_en.sh
+++ WEAT/fasttext_en.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+## COMMAND LINE ARGUMENTS
+# $1: word embeddings in w2v format
+# $2: save directory for WEAT results
+
+
+## arguments for weat.py
 #parser.add_argument("--test_number", type=int, help="Number of the weat test to run", required=False)
 #parser.add_argument("--permutation_number", type=int, default=None,
 #                     help="Number of permutations (otherwise all will be run)", required=False)
@@ -8,6 +15,7 @@
 #                      required=False)
 #  parser.add_argument("--embedding_file", type=str)
 
+
 #for similarity_type in "cosine" "csls" ; do
 for similarity_type in "cosine" ; do
     for test_number in 6 7 8 ; do
@@ -16,11 +24,11 @@ for similarity_type in "cosine" ; do
         python WEAT/XWEAT/weat.py \
             --test_number $test_number \
             --permutation_number 1000000 \
-            --output_file /disk/scratch/s1303513/weato6/fasttext_en_${similarity_type}_${test_number}.res \
+            --output_file $2/fasttext_en_${similarity_type}_${test_number}.res \
             --lower False \
             --use_glove False \
 	    --is_vec_format True \
-            --embeddings /disk/scratch/s1303513/weato6/ft_o_8_w2v.txt \
+            --embeddings $1 \
             --similarity_type $similarity_type |& tee ./results/fasttext_en_${similarity_type}_${test_number}.out
     done
 done
