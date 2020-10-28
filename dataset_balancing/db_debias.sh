c1441c828d1ce2f9765929caf0a7608e43ef274d
r-marchant <rmarch218@gmail.com>
2020-10-28 17:00:43 +0000

    cleaned code

--- dataset_balancing/db_debias.sh
+++ dataset_balancing/db_debias.sh
@@ -1,33 +1,49 @@
 #!/usr/bin/env bash
 
+### COMMAND LINE ARGUMENTS
+# $1: original dataset
+# $2: path to save file for balanced dataset
+
 # set to fail at first error
 set -o errexit
+
+export STUDENT_ID=$(whoami)
 
 # source my bashrc
 source ~/.bashrc
 
 conda activate bias_env
 
-mkdir -p /disk/scratch/s1303513
+mkdir -p /disk/scratch/${STUDENT_ID}/db_debias
 
 echo Copying data to scratch space
 
-rsync -av ./data/wiki_new_final_copy.txt /disk/scratch/s1303513/wiki_new_final_copy.txt
+rsync -av $1 /disk/scratch/${STUDENT_ID}/db_debias/original_data.txt
 
-echo Executing python script
+echo Executing python script for WEAT test 6
 
-python ./git2/embedding_bias/dataset_balancing/db_debias.py /disk/scratch/s1303513/wiki_new_final_copy.txt /disk/scratch/s1303513/db_debias_data_7.txt 7
+python ./git2/embedding_bias/dataset_balancing/db_debias.py /disk/scratch/${STUDENT_ID}/db_debias/original_data.txt /disk/scratch/${STUDENT_ID}/db_debias/db_debias_data_6.txt 6
 
-echo Copying data back to home directory
+echo Copying WEAT 6 data back to home directory
 
-rsync -av /disk/scratch/s1303513/db_debias_data_7.txt ./db_debias_data_7.txt
+rsync -av /disk/scratch/${STUDENT_ID}/db_debias/db_debias_data_6.txt $2/db_debias_data_6.txt
 
-echo Executing second python script
+echo Executing python script for WEAT test 7
 
-python ./git2/embedding_bias/dataset_balancing/db_debias.py /disk/scratch/s1303513/wiki_new_final_copy.txt /disk/scratch/s1303513/db_debias_data_8.txt 8
+python ./git2/embedding_bias/dataset_balancing/db_debias.py /disk/scratch/${STUDENT_ID}/db_debias/original_data.txt /disk/scratch/${STUDENT_ID}/db_debias/db_debias_data_7.txt 7
 
-echo Copying data back to home directory
+echo Copying WEAT 7 data back to home directory
 
-rsync -av /disk/scratch/s1303513/db_debias_data_7.txt ./db_debias_data_7.txt
+rsync -av /disk/scratch/${STUDENT_ID}/db_debias/db_debias_data_7.txt $2/db_debias_data_7.txt
 
-rsync -av /disk/scratch/s1303513/db_debias_data_8.txt ./db_debias_data_8.txt
+echo Executing python script for WEAT test 8
+
+python ./git2/embedding_bias/dataset_balancing/db_debias.py /disk/scratch/${STUDENT_ID}/db_debias/original_data.txt /disk/scratch/${STUDENT_ID}/db_debias/db_debias_data_8.txt 8
+
+echo Copying WEAT 8 data back to home directory
+
+rsync -av /disk/scratch/${STUDENT_ID}/db_debias/db_debias_data_8.txt $2/db_debias_data_8.txt
+
+echo Removing data from scratch space
+
+rm -r /disk/scratch/${STUDENT_ID}/db_debias
--- dataset_balancing/db_overbias.sh
+++ dataset_balancing/db_overbias.sh
@@ -1,33 +1,47 @@
 #!/usr/bin/env bash
 
+### COMMAND LINE ARGUMENTS
+# $1: original dataset
+# $2: path to save file for balanced dataset
+
 # set to fail at first error
 set -o errexit
+
+export STUDENT_ID=$(whoami)
 
 # source my bashrc
 source ~/.bashrc
 
 conda activate bias_env
 
-mkdir -p /disk/scratch/s1303513/overbias
+mkdir -p /disk/scratch/${STUDENT_ID}/db_overbias
 
 echo Copying data to scratch space
 
-rsync -av ./data/wiki_new_final_copy.txt /disk/scratch/s1303513/overbias/wiki_new_final_copy.txt
+rsync -av $1 /disk/scratch/${STUDENT_ID}/db_overbias/original_data.txt
 
-echo Executing python script
+echo Executing python script for WEAT test 6
 
-python ./git2/embedding_bias/dataset_balancing/db_overbias.py /disk/scratch/s1303513/overbias/wiki_new_final_copy.txt /disk/scratch/s1303513/overbias/db_overbias_data_7.txt 7
+python ./git2/embedding_bias/dataset_balancing/db_overbias.py /disk/scratch/${STUDENT_ID}/db_overbias/original_data.txt /disk/scratch/${STUDENT_ID}/db_overbias/db_overbias_data_6.txt 6
 
-echo Copying data back to home directory
+echo Copying WEAT 6 data back to home directory
 
-rsync -av /disk/scratch/s1303513/overbias/db_overbias_data_7.txt ./db_overbias_data_7.txt
+rsync -av /disk/scratch/${STUDENT_ID}/db_overbias/db_overbias_data_6.txt $2/db_overbias_data_6.txt
 
-echo Executing python script
+echo Executing python script for WEAT test 7
 
-python ./git2/embedding_bias/dataset_balancing/db_overbias.py /disk/scratch/s1303513/overbias/wiki_new_final_copy.txt /disk/scratch/s1303513/overbias/db_overbias_data_8.txt 8
+python ./git2/embedding_bias/dataset_balancing/db_overbias.py /disk/scratch/${STUDENT_ID}/db_overbias/original_data.txt /disk/scratch/${STUDENT_ID}/db_overbias/db_overbias_data_7.txt 7
 
-echo Copying data back to home directory
+echo Copying WEAT 7 data back to home directory
 
-rsync -av /disk/scratch/s1303513/overbias/db_overbias_data_8.txt ./db_overbias_data_8.txt
+rsync -av /disk/scratch/${STUDENT_ID}/db_overbias/db_overbias_data_7.txt $2/db_overbias_data_7.txt
 
-rm /disk/scratch/s1303513/overbias/*
+echo Executing python script for WEAT test 8
+
+python ./git2/embedding_bias/dataset_balancing/db_overbias.py /disk/scratch/${STUDENT_ID}/db_overbias/original_data.txt /disk/scratch/${STUDENT_ID}/db_overbias/db_overbias_data_8.txt 8
+
+echo Copying WEAT 8 data back to home directory
+
+rsync -av /disk/scratch/${STUDENT_ID}/db_overbias/db_overbias_data_8.txt $2/db_overbias_data_8.txt
+
+rm /disk/scratch/${STUDENT_ID}/db_overbias/*
